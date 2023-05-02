40c492d3dc0c3cd3db4524462902a112e395b606
Angel Morales <angelmorales@gmail.com>
2023-05-01 23:05:25 -0400

    First

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import { useFetch } from "./useFetch";
+import './App.css';
+
+function App() {
+  const {data, loading, error, handleCancelRequest } = useFetch(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+
+  return (
+    <div className="App">
+      <h1>Fetch like PRO</h1>
+      <button onClick={handleCancelRequest}>Cancel Request</button>
+      <div className= "card">
+        <ul>
+          {error && <li>Error: {error}</li>} 
+          {loading && <li>Loading...</li>}
+          {data?.map((user) => (
+            <li Key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+      
+export default App;
